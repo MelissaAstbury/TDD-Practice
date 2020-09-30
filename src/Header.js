18747c5cae2bb2a927d68dffabba078aef691d27
Melissa Astbury <melissaastbury@hotmail.com>
2020-09-30 15:51:56 +0100

    made a new test to show if the Header is being rendered. If the component is commented out this will fail but with the component rendering the test will pass

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <p>This is the header</p>
+        </div>
+    )
+};
+
+export default Header;
---
+++ src/Header.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Header from './Header';
+
+test('renders learn header component', () => {
+    const { getByText } = render(<Header />);
+    const linkElement = getByText(/This is the header/i);
+    expect(linkElement).toBeInTheDocument();
+  });

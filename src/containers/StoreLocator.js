d85da6e343c654bbed3697035328f3fcec2ea8d7
Melissa Astbury <melissaastbury@hotmail.com>
2020-10-01 11:07:50 +0100

    written tests for checking the following: rendering the StoreLocator, renders the Header, renders the Button(s) & renders the Map

--- src/containers/StoreLocator.js
+++ src/containers/StoreLocator.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import SoloMap from "../components/SoloMap";
 
 const StoreLocator = () => {
     return (
         <div>
-            <p>This is the Store Locator</p>
+            <Header />
+            <Button />
+            <Button />
+            <SoloMap />
         </div>
     )
 };
--- src/containers/__tests__/storeLocator.test.js
+++ src/containers/__tests__/storeLocator.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import StoreLocator from '../StoreLocator';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<StoreLocator />);
-  const linkElement = getByText(/This is the Store Locator/i);
-  expect(linkElement).toBeInTheDocument();
+describe('StoreLocator', () => {
+
+let mountedStoreLocator;
+beforeEach(() => {
+  mountedStoreLocator = shallow(<StoreLocator/>);
+});
+
+it("renders without crashing", () => {
+  let mountedStoreLocator = shallow(<StoreLocator />);
+});
+
+it("renders a header", () => {
+  const headers = mountedStoreLocator.find("Header");
+  expect(headers.length).toBe(1);
+});
+
+it("renders two buttons", () => {
+  const buttons = mountedStoreLocator.find("Button");
+  expect(buttons.length).toBe(2);
+})
+
+it("renders the map", () => {
+  const maps = mountedStoreLocator.find("SoloMap");
+  expect(maps.length).toBe(1);
+})
+  // const { getByText } = render(<StoreLocator />);
+  // const linkElement = getByText(/This is the Store Locator/i);
+  // expect(linkElement).toBeInTheDocument();
 });

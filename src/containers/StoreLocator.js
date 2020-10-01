f23fc7d9d20943a6a3696e3bba13f516fc797e0e
Melissa Astbury <melissaastbury@hotmail.com>
2020-10-01 18:20:52 +0100

    implemented used state to refactor the code for Button

--- src/containers/StoreLocator.js
+++ src/containers/StoreLocator.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import SoloMap from "../components/SoloMap";
 
 const StoreLocator = () => {
+  const [shops, setShops] = useState([
+    {
+      location: "Portland",
+      address: "123 Portland Drive",
+    },
+    {
+      location: "Astoria",
+      address: "123 Astoria Drive",
+    },
+    {
+      location: "",
+      address: "",
+    },
+  ]);
+
   return (
     <div>
       <Header />
       <div>
-        <Button location="Portland" />
-        <Button location="Astoria" />
-        <Button />
+        {shops.map((shop, id) => (
+          <Button key={id} location={shop.location} />
+        ))}
       </div>
       <SoloMap />
     </div>
